c553b62aad5efe2a988c567fa92d8a4cafdefca3
Larry Botha <larry@fixate.it>
2018-04-06 08:34:07 +0200

    begin 05

---
+++ 05.js
@@ -0,0 +1,10 @@
+const exhanges = [
+  [
+    {symbol: 'ZXC', price: 213.4, volume: 12345},
+    {symbol: 'GXC', price: 12.4, volume: 2345},
+  ],
+  [
+    {symbol: 'ZCB', price: 123.4, volume: 21345},
+    {symbol: 'GXR', price: 1.24, volume: 2354},
+  ],
+];
