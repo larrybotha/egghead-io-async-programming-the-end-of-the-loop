ac4d8f57fb17705b20b79c54d1646c5b14770d8f
Larry Botha <larry@fixate.it>
2018-04-06 08:46:29 +0200

    rewrite concatall using reduce

--- 05.js
+++ 05.js
@@ -1,4 +1,4 @@
-const exhanges = [
+const exchanges = [
   [
     {symbol: 'ZXC', price: 213.4, volume: 12345},
     {symbol: 'GXC', price: 12.4, volume: 2345},
@@ -8,3 +8,15 @@ const exhanges = [
     {symbol: 'GXR', price: 1.24, volume: 2354},
   ],
 ];
+
+// add concatAll directly to the Array object
+Array.prototype.concatAll = function concatAll() {
+  // let results = [];
+
+  // Will only flatten a 2-dimensional array
+  // this.forEach(subArray => subArray.forEach(item => results.push(item)));
+
+  return this.reduce((acc, subArray) => acc.concat(...subArray), []);
+};
+
+module.exports = exchanges;
