f413da323ede966a770cb986cfec82c7472e3a70
Larry Botha <larry@fixate.it>
2018-04-08 08:48:53 +0200

    add sync try-catch example

--- 06/index.js
+++ 06/index.js
@@ -7,3 +7,17 @@ const button = document.querySelector('.js-button');
 // };
 
 // button.addEventListener('click', handleClick);
+// What we're doing is changing thinking about events as objects with properties,
+// and instead treat them as first-class collections
+
+// Arrays are syncrhonous.
+// In order to execute something after an array, we simply add the line to
+// execute after the array.
+// To catch an error, we place it in a try-catch block
+try {
+  [1, 2, 3].forEach(n => console.log(n));
+
+  console.log('done');
+} catch (e) {
+  console.log('error');
+}
