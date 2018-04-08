56eb18213031a3a9d48b464362b01f67ced966ee
Larry Botha <larry@fixate.it>
2018-04-08 08:48:32 +0200

    add event listener pattern

---
+++ 06/index.js
@@ -0,0 +1,9 @@
+const button = document.querySelector('.js-button');
+
+// traditional event handler pattern for responding to user events
+// const handleClick = () => {
+//   alert('clicked');
+//   button.removeEventListener('click', handleClick);
+// };
+
+// button.addEventListener('click', handleClick);
