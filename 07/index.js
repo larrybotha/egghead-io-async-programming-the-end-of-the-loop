a26989651a9d715faee083fb22867431ed220370
Larry Botha <larry@fixate.it>
2018-04-09 09:24:13 +0200

    subscribe to points, and add note on lazy instantiation

--- 07/index.js
+++ 07/index.js
@@ -2,6 +2,13 @@ const button = document.querySelector('.js-button');
 
 const click$ = Rx.Observable.fromEvent(button, 'click');
 
+// instead of subscribing to the click stream, we subscribe to the point stream
+// Before we subscribe to this observable no click handlers are added to the
+// DOM. Obervables are instantiated lazily - they don't add any interaction or
+// intent until they are going to be consumed with something like subscribe.
+const subscription = point$.subscribe(
+  function onNext(item) {
+    alert(`clicked ${JSON.stringify(item, null, 2)}`);
 
     subscription.unsubscribe();
   },
