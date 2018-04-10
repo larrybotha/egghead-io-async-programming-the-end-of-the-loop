ecd38a3f675dd1e8557f5ee55b0a2c93122d7ed3
Larry Botha <larry@fixate.it>
2018-04-10 08:06:56 +0200

    setup

--- 08/index.js
+++ 08/index.js
@@ -1,18 +1,10 @@
-const button = document.querySelector('.js-button');
+const {Observable} = Rx;
 
-const click$ = Rx.Observable.fromEvent(button, 'click');
-const point$ = click$.map(({clientX, clientY}) => ({x: clientX, y: clientY}));
+const parent = document.getElementById('parent');
+const widget = document.getElementById('widget');
 
-const subscription = point$.subscribe(
-  function onNext(item) {
-    alert(`clicked ${JSON.stringify(item, null, 2)}`);
 
-    subscription.unsubscribe();
   },
-  function onError(err) {
-    console.log('error', err);
   },
-  function onComplete() {
-    console.log('done');
   }
 );
