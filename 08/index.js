707197dc9caa9854c2e2c584e32f9a36c5bf2a18
Larry Botha <larry@fixate.it>
2018-04-10 08:07:11 +0200

    create base streams / observables

--- 08/index.js
+++ 08/index.js
@@ -3,7 +3,13 @@ const {Observable} = Rx;
 const parent = document.getElementById('parent');
 const widget = document.getElementById('widget');
 
+// create, from the mousedown event, an observable stream on the widget
+const mouseDown$ = Observable.fromEvent(widget, 'mousedown');
 
+// create, from the mousemove event, an observable stream on the parent
+const parentMouseMove$ = Observable.fromEvent(parent, 'mousemove');
+// and the same for the mouseup event
+const parentMouseUp$ = Observable.fromEvent(parent, 'mouseup');
   },
   },
   }
